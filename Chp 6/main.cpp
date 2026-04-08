07162d9189dc785d9c28f0b6f7ca79a9ad58b391
ththfql <jekkilekki@gmail.com>
2026-04-08 17:22:07 +0900

    Student program not working

--- Chp 6/main.cpp
+++ Chp 6/main.cpp
@@ -1,8 +1,52 @@
 ﻿// main.cpp
+#include <algorithm>
+#include <fstream>
+#include <ios>
+#include <iomanip>
 #include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
-// 새로운 시도...
+#include "grade.h"
+#include "median.h"
+#include "Student_info.h"
+
+using namespace std;
+
 int main(void) {
-	std::cout << "이 거 할 수 있나???" << std::endl;
-	return 0;
+	// 파일 스트림 만들고 "txt" 파일을 읽기
+	ifstream student_file("grades.txt"); 
+
+	Student_info std;
+	string::size_type maxlen = 0;
+
+	// 모든 과제를 제출한 학생과 그렇지 않은 학생
+	vector<Student_info> did, didnt;
+
+	// 학생 이름과 모든 점수를 읽어 저장하고
+	// 가장 긴 이름을 찾음.
+	while (read(student_file, std)) {
+		// cin을 사용하면 직접 사용자 입력만 받을 수 있다.
+		// student_file은 ifstream이서어 파일 입력 받을 수 있다.
+
+		maxlen = max(maxlen, std.name.size());
+
+		if (did_all_hw(std))
+			did.push_back(std);
+		else
+			didnt.push_back(std);
+
+		// 두 집단에 데이터가 있는지 각각
+		// 확인하여 분석할 필요가 있는지 확인
+		if (did.empty()) {
+			cout << "No student did all homework!" << endl;
+			return 1; // 오류 코드
+		}
+		if (didnt.empty()) {
+			cout << "All students did all homework!" << endl;
+		}
+
+		return 0;
+	}
 }
